9b661dc7136afff601b4f914bbf843a1f6592e9b
Jagadeesh gongidi <jagadeeshgoud001@gmail.com>
2023-04-17 01:15:21 +0530

    added onboardingDataFlow Component (Multistep Form)

--- src/App.jsx
+++ src/App.jsx
@@ -114,94 +114,162 @@ const products = [
 
 
 
-const App = () => {
+// const App = () => {
+//   return (
+//     <div>
+//       <RegularList items={people} resourceName="person" itemComponent={SmallPeopleListItem} />
+//       <NumberedListItem items={people} resourceName="person" itemComponent={LargePeopleListItem} />
+
+//       <NumberedListItem items={products} resourceName="product" itemComponent={LargeProductLIstItem} />
+
+//       <RegularList items={products} resourceName="product" itemComponent={SmallProductListItem} />
+//     </div>
+//   )
+// }
+
+
+
+
+// //modal
+// const getServerData = url => async () => {
+//   const response = await axios.get(url)
+//   return response.data
+// }
+// const getLocalData = key => () => {
+//   console.log(key)
+//   return localStorage.getItem(key)
+// }
+
+
+// const Text = ({ message }) => {
+//   console.log("text", message)
+//   return <h1>{message}</h1>
+// }
+
+
+// const App = () => {
+
+//   return (
+//     <>
+//       {/* <RegularList items={people} resourceName="user" itemComponent={SmallPeopleListItem} />
+//       <Modal>
+//         <ProductInfo product={products[0]} />
+//       </Modal> */}
+//       {/* 
+//       <ResourceLoader resourceName="user" resourceUrl="api/users/1">
+//         <UserInfo />
+//       </ResourceLoader>
+//       <ResourceLoader resourceName="product" resourceUrl="api/products/1">
+//         <ProductInfo />
+//       </ResourceLoader> */}
+
+
+
+//       <DataSource getDataFunc={getServerData("/api/users/1")} resourceName='user'>
+//         <UserInfo />
+//       </DataSource>
+
+//       <DataSource getDataFunc={getLocalData("message")} resourceName='message'>
+//         <Text />
+//       </DataSource>
+
+//     </>
+//   )
+// }
+
+
+
+// export default App
+
+
+
+// import UnControlledForm from "./Components/uncontrolledcomponents/UnControlledForm";
+// import ControlledForm from "./Components/controlledcomponents/ControlledForm";
+// import UnControlledModel from "./Components/uncontrolledcomponents/UnControlledModel"
+// import ControlledModel from "./Components/controlledcomponents/ControlledModel"
+// import React, { useState } from "react";
+// const App = () => {
+//   const [shouldShowModal, setShowShouldShowModal] = useState(false)
+//   const toggleModel = () => {
+//     setShowShouldShowModal(!shouldShowModal)
+//   }
+
+//   return (
+//     <>
+//       <button onClick={toggleModel}>{shouldShowModal ? "hide model" : "Show model"}</button>
+//       <ControlledModel onRequestClose={toggleModel} shouldShow={shouldShowModal} >
+//         <h1>HI welcome to design patterns </h1>
+//       </ControlledModel >
+//     </>
+//   )
+// }
+// export default App
+import React, { useState } from "react";
+
+
+const Heading1 = ({ handleNext }) => {
+  const [name, setName] = useState("")
   return (
-    <div>
-      <RegularList items={people} resourceName="person" itemComponent={SmallPeopleListItem} />
-      <NumberedListItem items={people} resourceName="person" itemComponent={LargePeopleListItem} />
+    <>
+      <h1>First step</h1>
+      <input type="text" value={name} placeholder="enter name" onChange={(e) => setName(e.target.value)} />
+      <button onClick={() => handleNext({ name })}>Next</button>
+    </>
+  )
+}
 
-      <NumberedListItem items={products} resourceName="product" itemComponent={LargeProductLIstItem} />
-
-      <RegularList items={products} resourceName="product" itemComponent={SmallProductListItem} />
-    </div>
+const Heading2 = ({ handleNext }) => {
+  const [age, setAge] = useState(0)
+  return (
+    <>
+      <h1>second step</h1>
+      <input type="number" value={age} onChange={(e) => setAge(e.target.value)} />
+      <button onClick={() => handleNext({ age })}>Next</button>
+    </>
   )
 }
 
 
+const Heading3 = ({ handleNext }) => {
+  const [hairColor, setHairColor] = useState("")
 
-
-//modal
-const getServerData = url => async () => {
-  const response = await axios.get(url)
-  return response.data
-}
-const getLocalData = key => () => {
-  console.log(key)
-  return localStorage.getItem(key)
-}
-
-
-const Text = ({ message }) => {
-  console.log("text", message)
-  return <h1>{message}</h1>
-}
-
-
-
-const App = () => {
 
   return (
     <>
-      {/* <RegularList items={people} resourceName="user" itemComponent={SmallPeopleListItem} />
-      <Modal>
-        <ProductInfo product={products[0]} />
-      </Modal> */}
-      {/* 
-      <ResourceLoader resourceName="user" resourceUrl="api/users/1">
-        <UserInfo />
-      </ResourceLoader>
-      <ResourceLoader resourceName="product" resourceUrl="api/products/1">
-        <ProductInfo />
-      </ResourceLoader> */}
-
-
-
-      <DataSource getDataFunc={getServerData("/api/users/1")} resourceName='user'>
-        <UserInfo />
-      </DataSource>
-
-      <DataSource getDataFunc={getLocalData("message")} resourceName='message'>
-        <Text />
-      </DataSource>
-
+      <h1>third step</h1>
+      <input type="text" placeholder="haircolor.." value={hairColor} onChange={(e) => setHairColor(e.target.value)} />
+      <button onClick={() => handleNext({ hairColor })}>Next</button >
     </>
   )
 }
 
 
 
-export default App
 
 
 
-import UnControlledForm from "./Components/uncontrolledcomponents/UnControlledForm";
-import ControlledForm from "./Components/controlledcomponents/ControlledForm";
-import UnControlledModel from "./Components/uncontrolledcomponents/UnControlledModel"
-import ControlledModel from "./Components/controlledcomponents/ControlledModel"
-import React, { useState } from "react";
+
+
+import UnControlledOnBoardingFlow from "./Components/uncontrolledcomponents/UnControlledOnBoardingFlow"
 const App = () => {
-  const [shouldShowModal, setShowShouldShowModal] = useState(false)
-  const toggleModel = () => {
-    setShowShouldShowModal(!shouldShowModal)
+  const onFinish = (data) => {
+    alert("onBoarding is Completed")
+    console.log(data)
   }
 
   return (
     <>
-      <button onClick={toggleModel}>{shouldShowModal ? "hide model" : "Show model"}</button>
-      <ControlledModel onRequestClose={toggleModel} shouldShow={shouldShowModal} >
-        <h1>HI welcome to design patterns </h1>
-      </ControlledModel >
+
+      <UnControlledOnBoardingFlow OnFinish={onFinish}>
+        <Heading1 />
+        <Heading2 />
+        <Heading3 />
+
+      </UnControlledOnBoardingFlow>
+
+
     </>
   )
 }
-export default App
+
+export default App  
--- src/Components/uncontrolledcomponents/UnControlledOnBoardingFlow.jsx
+++ src/Components/uncontrolledcomponents/UnControlledOnBoardingFlow.jsx
@@ -1,22 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const UnControlledOnBoardingFlow = ({ children, OnFinish }) => {
     const [onBoardingData, setOnBoardingData] = useState({});
     const [currentIndex, setCurrentIndex] = useState(0);
     const currentChild = React.Children.toArray(children)[currentIndex];
-    
-    const isLastChild = currentIndex === React.Children.count(children) - 1;
-    const handleNext = data => {
-        setOnBoardingData({ ...onBoardingData, ...data });
+
+    const handleNext = StepData => {
+        const nextIndex = currentIndex + 1;
+        const upDatedData = { ...onBoardingData, ...StepData }
         setCurrentIndex(currentIndex + 1);
+        if (nextIndex < children.length) {
+            setCurrentIndex(nextIndex)
+        } else {
+            OnFinish(upDatedData)
+        }
+        setOnBoardingData(upDatedData)
     }
+
+
     const handleBack = () => {
         setCurrentIndex(currentIndex - 1);
     }
 
+
     if (React.isValidElement(currentChild)) {
-        return React.cloneElement(currentChild, { handleNext })
+        return React.cloneElement(currentChild, { handleNext, handleBack })
     }
+
+
+
     return currentChild
 }
 
